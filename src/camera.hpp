2b2347fb9c40fe22800968c9db6fe44012aacf4f
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2021-03-14 15:49:54 -0700

    [brief] Sets up a simple camera.
    [detailed]

---
+++ src/camera.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ray.hpp"
+
+template<std::size_t width, std::size_t height>
+class Camera
+{
+public:
+    constexpr math::Rayf get_ray(std::size_t row, std::size_t col) const
+    {
+        float u = static_cast<float>(row) / (width - 1);
+        float v = static_cast<float>(col) / (height - 1);
+
+        math::Rayf ray{m_origin,
+                       m_lower_left + (u * m_right) + (v * m_up) - m_origin};
+        return ray;
+    }
+
+private:
+    float m_viewport_height{2.0f};
+    float m_aspect_ratio{static_cast<float>(width) / height};
+    float m_viewport_width{m_aspect_ratio * m_viewport_height};
+    float m_focal_length{1.0f};
+
+    math::Vectorf m_origin{0.0f};
+    math::Vectorf m_right{m_viewport_width, 0.0f, 0.0f};
+    math::Vectorf m_up{0.0f, m_viewport_height, 0.0f};
+    math::Vectorf m_lower_left{m_origin - (m_right / 2.0f) - (m_up / 2.0f) -
+                               math::Vectorf{0.0f, 0.0f, m_focal_length}};
+};
---
+++ test/camera_test.cpp
@@ -0,0 +1,12 @@
+#include "camera.hpp"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("[Camera] - get_ray")
+{
+    constexpr Camera<100, 100> camera;
+    constexpr auto result = camera.get_ray(0, 0);
+
+    REQUIRE(result.origin == math::Vectorf{0.0f});
+    REQUIRE(result.direction == math::Vectorf{-1.0f});
+}
