92a350694d9a735ab46631b9b905430c563c9518
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2021-03-16 17:48:10 -0700

    [brief] Adds a few utilities.
    [detailed]
    - Adds a base class for CRTP.
    - Adds a way to check if a provided template is in a parameter pack.

--- src/utils.hpp
+++ src/utils.hpp
@@ -7,6 +7,7 @@ namespace utils
 {
     namespace detail
     {
+        // Adapted from https://gist.github.com/alexshtf/eb5128b3e3e143187794
         template<typename T>
         constexpr T sqrt_newton_rhapson(T x, T curr, T prev)
         {
@@ -26,4 +27,29 @@ namespace utils
 
         return std::numeric_limits<T>::quiet_NaN();
     }
+
+    // Taken from FluentC++
+    // https://www.fluentcpp.com/2017/05/19/crtp-helper/
+    template<typename T, template<typename> class CRTPType>
+    class CRTP
+    {
+        CRTP() = default;
+        friend CRTPType<T>;
+
+    public:
+        T& underlying()
+        {
+            return static_cast<T&>(*this);
+        }
+
+        T const& underlying() const
+        {
+            return static_cast<T const&>(*this);
+        }
+    };
+
+    template<typename T, typename... Args>
+    struct Contains
+        : public std::bool_constant<(std::is_same<T, Args>{} || ...)>
+    {};
 } // namespace utils
