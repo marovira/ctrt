266c2b25b52b088b0ccbfedaa5c97e404c7cc066
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2021-04-11 12:37:48 -0700

    [brief] Adds Phong and Reflective materials.
    [detailed]
    - Also adds the corresponding BRDFs.

--- src/brdf.hpp
+++ src/brdf.hpp
@@ -3,6 +3,7 @@
 #include "shapes.hpp"
 #include "utils.hpp"
 
+#include <tuple>
 #include <variant>
 #include <zeus/constants.hpp>
 
@@ -24,6 +25,12 @@ public:
     {
         return this->self().rho(rec, reflected);
     }
+
+    constexpr std::tuple<Colour, Vector> sample_fn(ShadeRec const& sr,
+                                                   Vector const& wo) const
+    {
+        return this->self().sample_fn(sr, wo);
+    }
 };
 
 class Lambertian : public BRDF<Lambertian>
@@ -43,6 +50,12 @@ public:
         return m_diffuse_colour * m_diffuse_reflection;
     }
 
+    constexpr std::tuple<Colour, Vector> sample_fn(ShadeRec const&,
+                                                   Vector const&)
+    {
+        return {Colour{}, Vector{}};
+    }
+
     constexpr void set_diffuse_reflection(float refl)
     {
         m_diffuse_reflection = refl;
@@ -56,6 +69,103 @@ public:
 private:
     Colour m_diffuse_colour;
     float m_diffuse_reflection{0.0f};
+};
+
+class GlossySpecular : public BRDF<GlossySpecular>
+{
+public:
+    constexpr GlossySpecular()
+    {}
+
+    constexpr Colour
+    fn(ShadeRec const& rec, Vector const& wo, Vector const& wi) const
+    {
+        Colour L;
+        float n_dot_wi = dot(rec.normal, wi);
+        Vector r{-wi + 2.0f * rec.normal * n_dot_wi};
+        float r_dot_wo = dot(r, wo);
+
+        if (r_dot_wo > 0.0f)
+        {
+            L = m_specular_reflection * m_specular_colour *
+                utils::pow(r_dot_wo, m_specular_exponent);
+        }
+
+        return L;
+    }
+
+    constexpr Colour rho(ShadeRec const&, Vector const&) const
+    {
+        return Colour{};
+    }
+
+    constexpr std::tuple<Colour, Vector> sample_fn(ShadeRec const&,
+                                                   Vector const&)
+    {
+        return {Colour{}, Vector{}};
+    }
+
+    constexpr void set_specular_reflection(float ks)
+    {
+        m_specular_reflection = ks;
+    }
+
+    constexpr void set_specular_exponent(int exp)
+    {
+        m_specular_exponent = exp;
+    }
+
+    constexpr void set_specular_colour(Colour const& col)
+    {
+        m_specular_colour = col;
+    }
+
+private:
+    float m_specular_reflection{0.0f};
+    int m_specular_exponent{1};
+    Colour m_specular_colour{1.0f};
+};
+
+class PerfectSpecular : public BRDF<PerfectSpecular>
+{
+public:
+    constexpr PerfectSpecular()
+    {}
+
+    constexpr Colour fn(ShadeRec const&, Vector const&, Vector const&)
+    {
+        return Colour{};
+    }
+
+    constexpr Colour rho(ShadeRec const&, Vector const&)
+    {
+        return Colour{};
+    }
+
+    constexpr std::tuple<Colour, Vector> sample_fn(ShadeRec const& sr,
+                                                   Vector const& wo) const
+    {
+        float n_dot_wo = dot(sr.normal, wo);
+        Vector out_vec = -wo + 2.0f * sr.normal * n_dot_wo;
+        Colour out_col =
+            m_reflection_coeff * m_reflection_colour / dot(sr.normal, out_vec);
+
+        return {out_col, out_vec};
+    }
+
+    constexpr void set_reflection_coeff(float kr)
+    {
+        m_reflection_coeff = kr;
+    }
+
+    constexpr void set_reflection_colour(Colour const& col)
+    {
+        m_reflection_colour = col;
+    }
+
+private:
+    float m_reflection_coeff{0.0f};
+    Colour m_reflection_colour{0.0f};
 };
 
 template<class... BRDFs>
@@ -88,4 +198,4 @@ private:
     std::variant<BRDFs...> m_brdf;
 };
 
-using BRDFs = BRDFWrapper<Lambertian>;
+using BRDFs = BRDFWrapper<Lambertian, GlossySpecular, PerfectSpecular>;
--- src/materials.hpp
+++ src/materials.hpp
@@ -12,12 +12,15 @@ public:
     constexpr Material()
     {}
 
-    template<class LightContainer, class ShapeContainer>
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
     constexpr Colour shade(ShadeRec const& rec,
                            LightContainer const& lights,
-                           ShapeContainer const& shapes) const
+                           ShapeContainer const& shapes,
+                           MaterialContainer const& materials) const
     {
-        return this->self().shade(rec, lights, shapes);
+        return this->self().shade(rec, lights, shapes, materials);
     }
 };
 
@@ -27,10 +30,13 @@ public:
     constexpr EmptyMaterial()
     {}
 
-    template<class LightContainer, class ShapeContainer>
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
     constexpr Colour shade(ShadeRec const& rec,
                            LightContainer const&,
-                           ShapeContainer const&) const
+                           ShapeContainer const&,
+                           MaterialContainer const&) const
     {
         return rec.default_colour;
     }
@@ -42,10 +48,13 @@ public:
     constexpr Matte()
     {}
 
-    template<class LightContainer, class ShapeContainer>
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
     constexpr Colour shade(ShadeRec const& rec,
                            LightContainer const& lights,
-                           ShapeContainer const& shapes) const
+                           ShapeContainer const& shapes,
+                           MaterialContainer const&) const
     {
         Vector wo = -rec.ray.origin;
         Colour L  = m_ambient_brdf.rho(rec, wo) * lights[0].l(rec);
@@ -113,6 +122,193 @@ private:
     Lambertian m_ambient_brdf{};
 };
 
+class Phong : public Material<Phong>
+{
+public:
+    constexpr Phong()
+    {}
+
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
+    constexpr Colour shade(ShadeRec const& rec,
+                           LightContainer const& lights,
+                           ShapeContainer const& shapes,
+                           MaterialContainer const&) const
+    {
+        Vector wo = -rec.ray.direction;
+        Colour L  = m_ambient_brdf.rho(rec, wo) * lights[0].l(rec);
+
+        if (lights.size() == 1)
+        {
+            return L;
+        }
+
+        for (std::size_t i{1}; i < lights.size(); ++i)
+        {
+            Vector wi      = lights[i].get_direction(rec);
+            float n_dot_wi = dot(rec.normal, wi);
+
+            if (n_dot_wi > 0.0f)
+            {
+                bool in_shadow{false};
+                if (lights[i].casts_shadows())
+                {
+                    Ray shadow_ray{rec.hit_point, wi};
+                    float d =
+                        distance(shadow_ray.origin, lights[i].get_position());
+                    for (auto const& shape : shapes)
+                    {
+                        if (auto result = shape.shadow_hit(shadow_ray); result)
+                        {
+                            if (*result < d)
+                            {
+                                in_shadow = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (!in_shadow)
+                {
+                    L += (m_diffuse_brdf.fn(rec, wo, wi) +
+                          m_specular_brdf.fn(rec, wo, wi)) *
+                         lights[i].l(rec) * n_dot_wi;
+                }
+            }
+        }
+
+        return L;
+    }
+
+    constexpr void set_diffuse_reflection(float k)
+    {
+        m_diffuse_brdf.set_diffuse_reflection(k);
+    }
+
+    constexpr void set_ambient_reflection(float k)
+    {
+        m_ambient_brdf.set_diffuse_reflection(k);
+    }
+
+    constexpr void set_diffuse_colour(Colour const& colour)
+    {
+        m_ambient_brdf.set_diffuse_colour(colour);
+        m_diffuse_brdf.set_diffuse_colour(colour);
+    }
+
+    constexpr void set_specular_reflection(float ks)
+    {
+        m_specular_brdf.set_specular_reflection(ks);
+    }
+
+    constexpr void set_specular_exponent(int exp)
+    {
+        m_specular_brdf.set_specular_exponent(exp);
+    }
+
+    constexpr void set_specular_colour(Colour const& col)
+    {
+        m_specular_brdf.set_specular_colour(col);
+    }
+
+private:
+    Lambertian m_ambient_brdf{};
+    Lambertian m_diffuse_brdf{};
+    GlossySpecular m_specular_brdf{};
+};
+
+class Reflective : public Material<Reflective>
+{
+public:
+    constexpr Reflective()
+    {}
+
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
+    constexpr Colour shade(ShadeRec const& sr,
+                           LightContainer const& lights,
+                           ShapeContainer const& shapes,
+                           MaterialContainer const& materials) const
+    {
+        if (sr.reflection_depth > ShadeRec::max_reflection_depth)
+        {
+            return Colour{0.0f};
+        }
+
+        // Direct illumination first.
+        Colour L = m_phong.shade(sr, lights, shapes, materials);
+
+        Vector wo     = -sr.ray.direction;
+        auto [fr, wi] = m_reflective_brdf.sample_fn(sr, wo);
+        Ray reflected_ray{sr.hit_point, wi};
+
+        Colour reflected_colour;
+        for (auto const& shape : shapes)
+        {
+            if (auto result = shape.hit(reflected_ray); result)
+            {
+                auto rec             = *result;
+                rec.reflection_depth = sr.reflection_depth + 1;
+                reflected_colour     = materials[rec.material_id].shade(
+                    rec, lights, shapes, materials);
+                break;
+            }
+        }
+
+        L += fr * reflected_colour * dot(sr.normal, wi);
+
+        return L;
+    }
+
+    constexpr void set_diffuse_reflection(float k)
+    {
+        m_phong.set_diffuse_reflection(k);
+    }
+
+    constexpr void set_ambient_reflection(float k)
+    {
+        m_phong.set_diffuse_reflection(k);
+    }
+
+    constexpr void set_diffuse_colour(Colour const& colour)
+    {
+        m_phong.set_diffuse_colour(colour);
+        m_phong.set_diffuse_colour(colour);
+    }
+
+    constexpr void set_specular_reflection(float ks)
+    {
+        m_phong.set_specular_reflection(ks);
+    }
+
+    constexpr void set_specular_exponent(int exp)
+    {
+        m_phong.set_specular_exponent(exp);
+    }
+
+    constexpr void set_specular_colour(Colour const& col)
+    {
+        m_phong.set_specular_colour(col);
+    }
+
+    constexpr void set_reflection_coeff(float kr)
+    {
+        m_reflective_brdf.set_reflection_coeff(kr);
+    }
+
+    constexpr void set_reflection_colour(Colour const& col)
+    {
+        m_reflective_brdf.set_reflection_colour(col);
+    }
+
+private:
+    Phong m_phong;
+    PerfectSpecular m_reflective_brdf;
+};
+
 template<class... Materials>
 class MaterialWrapper
 {
@@ -124,14 +320,17 @@ public:
     constexpr MaterialWrapper(T&& elem) : m_material{std::forward<T>(elem)}
     {}
 
-    template<class LightContainer, class ShapeContainer>
+    template<class LightContainer,
+             class ShapeContainer,
+             class MaterialContainer>
     constexpr Colour shade(ShadeRec const& rec,
                            LightContainer const& lights,
-                           ShapeContainer const& shapes) const
+                           ShapeContainer const& shapes,
+                           MaterialContainer const& materials) const
     {
         return std::visit(
-            [rec, &lights, &shapes](auto const& elem) {
-                return elem.shade(rec, lights, shapes);
+            [rec, &lights, &shapes, &materials](auto const& elem) {
+                return elem.shade(rec, lights, shapes, materials);
             },
             m_material);
     }
@@ -140,4 +339,4 @@ private:
     std::variant<Materials...> m_material;
 };
 
-using Materials = MaterialWrapper<EmptyMaterial, Matte>;
+using Materials = MaterialWrapper<EmptyMaterial, Matte, Phong, Reflective>;
